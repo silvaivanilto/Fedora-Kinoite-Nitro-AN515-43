a214b7a03e2e7d2aa29bdd23146c28623ab93a95
Ivanilton <silvaivanilto@gmail.com>
2026-02-17 17:21:20 -0300

    fix: Use atomic rpm-ostree override remove --install for display manager swap

--- files/scripts/swap-display-manager.sh
+++ files/scripts/swap-display-manager.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -ouex pipefail
 
-# 1. Install Plasma Login Manager
-rpm-ostree install \
-    kcm-plasmalogin \
-    plasma-login-manager
-
-# 2. Enable Plasma Login Manager service
-systemctl enable plasmalogin.service
-
-# 3. Remove SDDM (safe now that Plasma Login is installed)
+# 1. Atomic swap: Remove SDDM + Install Plasma Login Manager in one transaction
 rpm-ostree override remove \
     kde-settings-sddm \
     sddm \
     sddm-breeze \
     sddm-kcm \
-    sddm-wayland-plasma
+    sddm-wayland-plasma \
+    --install kcm-plasmalogin \
+    --install plasma-login-manager
+
+# 2. Enable Plasma Login Manager service (safe now that SDDM is gone)
+systemctl enable plasmalogin.service
