f501c3b4d0a80e5f0c1a36eff8268b7049e53434
Ivanilton <silvaivanilto@gmail.com>
2026-02-17 22:21:38 -0300

    refactor: move dnf wrapper to /usr/local/bin

--- system_files/shared/etc/profile.d/dnf-dummy.sh
+++ system_files/shared/etc/profile.d/dnf-dummy.sh
@@ -1,6 +1,6 @@
 if [[ $- == *i* ]]; then
     # /usr/bin/dnf is a dummy wrapper that opens docs
-    alias pacman='/usr/bin/dnf'
-    alias apt='/usr/bin/dnf'
-    alias yum='/usr/bin/dnf'
+    alias pacman='/usr/local/bin/dnf'
+    alias apt='/usr/local/bin/dnf'
+    alias yum='/usr/local/bin/dnf'
 fi
