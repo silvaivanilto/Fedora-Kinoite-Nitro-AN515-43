d69bf0281ea23c3302182f84aac84b6bb742e0f3
Ivanilton <silvaivanilto@gmail.com>
2026-02-19 12:12:29 -0300

    chore: remove configure-nvidia.sh (handled by ublue base)

--- files/scripts/configure-nvidia.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ouex pipefail
-
-# Configure Nvidia Dynamic Power Management
-cat <<EOF > /etc/modprobe.d/nvidia.conf
-options nvidia "NVreg_DynamicPowerManagement=0x02"
-EOF
-
-chmod 644 /etc/modprobe.d/nvidia.conf
